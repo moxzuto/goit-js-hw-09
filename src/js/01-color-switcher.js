b38e5812328357497506b91faaeaf96543a20cc8
Ilya Kulish <ilyakulish06@outlook.com>
2023-04-07 01:28:53 +0400

    1 homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+function startChangingColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopChangingColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const body = document.body;
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
 
