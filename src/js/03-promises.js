a7d425c7f6f63991507df9bf5d1f162e7a189a32
Ilya Kulish <ilyakulish06@outlook.com>
2023-04-08 03:02:26 +0400

    task 3
    mb need work

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
